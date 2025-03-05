12e7a0e51ce6a8b5638a9f2312b1b3e8c52e09ac
lleballex <lleballex@gmail.com>
2025-03-05 11:27:10 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from '@/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('LeshaLove')
+    .setVersion('1.0')
+    .build()
+
+  SwaggerModule.setup('api', app, () =>
+    SwaggerModule.createDocument(app, swaggerConfig),
+  )
+
+  await app.listen(process.env.PORT)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
