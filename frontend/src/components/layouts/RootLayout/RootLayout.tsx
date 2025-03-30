5bf3061fda55511859861786d1c9e0de69db2db5
lleballex <lleballex@gmail.com>
2025-03-30 11:02:05 +0300

    feat: add react-router

--- frontend/src/components/layouts/RootLayout/RootLayout.tsx
+++ frontend/src/components/layouts/RootLayout/RootLayout.tsx
@@ -1,14 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-import Vacancies from '@/components/base/vacancy/Vacancies'
+import { Outlet } from 'react-router'
 
 const queryClient = new QueryClient()
 
-export default function App() {
+export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Vacancies />
+      <Outlet />
       <ReactQueryDevtools />
     </QueryClientProvider>
   )
---
+++ frontend/src/router.ts
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from 'react-router'
+
+import RootLayout from '@/components/layouts/RootLayout'
+import VacanciesPage from '@/components/pages/vacancies/VacanciesPage'
+import VacancyPage from '@/components/pages/vacancies/VacancyPage'
+import CreateVacancyPage from '@/components/pages/vacancies/CreateVacancyPage'
+import UpdateVacancyPage from '@/components/pages/vacancies/UpdateVacancyPage'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: RootLayout,
+    children: [
+      { index: true, Component: VacanciesPage },
+      {
+        path: 'new-vacancy',
+        Component: CreateVacancyPage,
+      },
+      {
+        path: 'vacancies/:id',
+        Component: VacancyPage,
+      },
+      {
+        path: 'vacancies/:id/edit',
+        Component: UpdateVacancyPage,
+      },
+    ],
+  },
+])
