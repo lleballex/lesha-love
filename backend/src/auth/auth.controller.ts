50ce6a79dc18aa88ef6350cac0a41d8b82a3b990
lleballex <lleballex@gmail.com>
2025-04-09 11:05:41 +0300

    feat: add auth/me endpoint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,35 @@
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common'
 import { ApiOperation } from '@nestjs/swagger'
+import { Request } from 'express'
+
+import { UsersService } from '@/users/users.service'
 
 import { LoginDto } from './dto/login.dto'
 import { RegisterDto } from './dto/register.dto'
 import { AuthService } from './auth.service'
+import { IsAuthedGuard } from './guards/is-authed.guard'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @Get('me')
+  @UseGuards(IsAuthedGuard)
+  @ApiOperation({ summary: 'Get authenticated user' })
+  me(@Req() req: Request) {
+    return this.usersService.findOne(req.user!.id)
+  }
 
   @Post('login')
   @ApiOperation({ summary: 'Login and generate access token' })
